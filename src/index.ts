f128e3e61ea4af7cc90ebc91a7be4e24b75bb025
Yoann Vernageau <y.vernageau@gmail.com>
2019-01-31 15:10:32 +0100

    add `ProfileValidator` and rename `debug` argument to `verbose`

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import {ProfileMapper} from './profile.mapper'
 import {Scheduler} from './scheduler'
 import {Watcher} from './watcher'
 import {Worker} from './worker'
-
-const logger = LoggerFactory.get('config')
+import { ProfileValidator } from './profile.validator';
 
 // Parse command arguments
 const args = yargs
@@ -14,21 +13,21 @@ const args = yargs
     .strict()
     .option('i', {
         alias: 'input',
-        desc: 'The input directory',
+        desc: 'Input directory',
         demandOption: true,
         type: 'string',
         nargs: 1
     })
     .option('o', {
         alias: 'output',
-        desc: 'The output directory',
+        desc: 'Output directory',
         demandOption: true,
         type: 'string',
         nargs: 1
     })
     .option('p', {
         alias: 'profile',
-        desc: 'The execution profile to apply for each input file',
+        desc: 'Execution profile to apply for each input file',
         demandOption: true,
         type: 'string',
         nargs: 1
@@ -39,39 +38,31 @@ const args = yargs
         type: 'boolean',
         default: false
     })
-    .option('debug', {
-        desc: 'Display debug information',
+    .option('v', {
+        alias: 'verbose',
+        desc: 'Makes console output more verbose',
         type: 'boolean',
         default: false
     })
     .parse()
 
+const logger = LoggerFactory.get('config')
 logger.info('profile = %s', args.profile)
 logger.info('input   = %s', args.input)
 logger.info('output  = %s', args.output)
 logger.info('watch   = %s', args.watch)
-logger.info('debug   = %s', args.debug)
+logger.info('verbose = %s', args.verbose)
 
+// Build and validate profile
 const profile: Profile = Profile.load(args.profile as string)
 profile.input.directory = args.input as string
 profile.output.directory = args.output as string
 
-LoggerFactory.debug = args.debug
+new ProfileValidator(profile).validate()
 
-// region Profile validation
+LoggerFactory.debug = args.verbose as boolean
 
-// Remove 'mappings' and 'mapping[].options' where 'skip === true'
-profile.output.mappings = profile.output.mappings.filter(m => !m.skip)
-profile.output.mappings.filter(m => m.options && m.options.length > 0).forEach(m => m.options = m.options.filter(o => !o.skip))
-
-if (!profile.input) throw new Error("Missing 'input' in profile")
-if (!profile.output) throw new Error("Missing 'output' in profile")
-if (!profile.input.include && !profile.input.exclude) throw new Error("Missing 'input.include' or 'input.exclude' in profile, all files are excluded by default")
-if (!profile.output.mappings || profile.output.mappings.length === 0) throw new Error("No 'output.mappings' defined")
-if (profile.output.mappings.some(m => !m.output)) throw new Error("'output' must be defined for each 'mappings'")
-
-// endregion
-
+// Initialize watcher
 const mapper: ProfileMapper = new ProfileMapper(profile)
 
 const scheduler = new Scheduler(profile, (file, callback) => {
